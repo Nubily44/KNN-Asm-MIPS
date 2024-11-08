ce76aebf73d8a4c0ca4d0b96aa977bfa1233595d
Kauê Oliveira <kauepatrickoliveira@gmail.com>
2024-11-08 00:18:07 -0300

    Adiciona loop de leitura do buffer

--- EP1_FINAL.asm
+++ EP1_FINAL.asm
@@ -1,12 +1,9 @@
 .data 
-    xTrain: .asciiz "Xtrain.txt" # Localização do arquivo de entrada (x) do conjunto de treino
-
-    xTest: .asciiz "Xtest.txt"  # Localização do arquivo de entrada (x) do conjunto de teste
-
-    yTrain: .asciiz "Ytrain.txt" # Localização do arquivo de entrada (y) do conjunto de treino
-
-    yTest: .asciiz "Ytest.txt" # Localização do arquivo de entrada (y) do conjunto de teste
-
+    xTrain: .asciiz "H:/Meu Drive/4� Semestre/Organiza��o e Arquitetura de Computadores II/EPs/EP1/KNN-Asm-MIPS/Xtrain.txt" # Localizacao do arquivo de entrada (x) do conjunto de treino
+    #xTrain: .asciiz "C:/Users/kauep/teste.txt"
+    #xTest: .asciiz "Xtest.txt"  # Localizacao do arquivo de entrada (x) do conjunto de teste
+    #yTrain: .asciiz "Ytrain.txt" # Localizacao do arquivo de entrada (y) do conjunto de treino
+    #yTest: .asciiz "Ytest.txt" # Localizacao do arquivo de entrada (y) do conjunto de teste
 
     buffer: .space 20000  
 
@@ -15,69 +12,91 @@
     v_yTrain: .space 40000
     v_xTest: .space 40000
     
-
-    dez: .double 10.0 
-    zero: .double 0.0
+    #dez: .double 10.0 
+    #zero: .double 0.0
     
-    masValue: .double 999999999999999
+    #masValue: .double 999999999999999
 
 .text
-
 .globl main
 
 main:
-    # Leitura dos arquivos - passa o caminho como parâmetro para a função lerArquivo
+    # Leitura dos arquivos - passa o caminho como parametro para a funcao lerArquivo
     
     la $a0, xTrain
     la $a3, v_xTrain
     jal lerArquivo
 
-    la $a0, xTest
-    la $a3, v_xTest
-    jal lerArquivo
+    #la $a0, xTest
+    #la $a3, v_xTest
+    #jal lerArquivo
 
-    la $a0, yTrain
-    la $a3, v_yTrain
-    jal lerArquivo
+    #la $a0, yTrain
+    #la $a3, v_yTrain
+    #jal lerArquivo
+
+    #la $s0, v_xTrain
+    #la $s1, v_yTrain
+    #la $s2, v_xTest
 
 
-    la $s0, v_xTrain
-    la $s1, v_yTrain
-    la $s2, v_xTest
-
-
-    move $a0, $v0
+    #move $a0, $v0
     #jal abrirArquivos
 
-    #j exit
+    j fim
 
-# Carrega o conteúdo de um arquivo no buffer
+# Carrega o conteudo de um arquivo no buffer
 lerArquivo:
-    # Abre o arquivo
+    # Abre o arquivo cujo caminho esta em $a0
     li $v0, 13
     li $a1, 0 # Flag 0: Read-only
-    li $a2, 0 # Modo que libera as permissões de acesso
-    syscall
+    li $a2, 0 # Modo que libera as permissoes de acesso
+    syscall # Salva o descritor do arquivo em v0 
+    addiu $a0, $v0, $zero # Move o descritor do arquivo (obtido com o syscall 13)
 
     # Carrega o arquivo no buffer
     li $v0, 14
-    move $a0, $v0 # Move o identificador do arquivo (obtido com o syscall 13)
-    la $a1, buffer # Endereço do buffer
-    li $a2, 20000 # Número máximo de caracteres a serem lidos
+    la $a1, buffer # Endereco do buffer
+    li $a2, 20000 # Numero maximo de caracteres a serem lidos
     syscall
 
     # Fecha o arquivo
     li $v0, 16
     syscall
+    
+    # TESTE-Imprime o buffer
+    #li $v0, 4
+    #la $a0, buffer
+    #syscall
 
 processarNum:
-    la $s0, ($a3) # Carrega o endereço do vetor onde os valores serão armazenados
+    la $s0, ($a3) # Carrega o endereco do vetor onde os valores serao armazenados
     la $s1, buffer # Carrega o buffer em si
+    li $t1, 0 # Inicializa o registrador que ir� conter o n�mero copiado
+    
+# Copia dos numeros
+copiaNumero:
+    lb $t0, 0($s1) # Carrega o primeiro byte do buffer em t0
+    
+    beq $t0, 0, fimArquivo # Verifica se o arquivo chegou ao fim
+    beq $t0, '\n', fimNumero # Verifica se ha quebra de linha - finaliza a copia do numero
+    
+    subu $t0, $t0, 48 # Converte o caractere para inteiro - o caractere '0' equivale ao n�mero 48 da tabela ASCII
+    
+    mul $t1, $t1, 10 # Multiplica o inteiro anteriormente armazenado por 10 (ha deslocamento de uma casa decimal)
+    add $t1, $t1, $t0 # Adiciona o digito atual
+    j fimCaractere
+    
+fimCaractere:
+    addiu $s1, $s1, 1 # Avan�a o buffer em um byte
+    j copiaNumero # Vai para a pr�xima itera��o do loop
 
-    # Cópia dos números
-    lb $t0, 0($s1) # Carrega o primeiro byte do buffer
+fimNumero:  
+      
+fimArquivo:
+    
+    
 
-
-exit: 
+fim: 
     li $v0, 10
     syscall
