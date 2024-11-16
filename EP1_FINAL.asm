b88b2147349a308fef37015b56efb717ee19cd25
Lily <alinecm@usp.br>
2024-11-16 03:45:57 -0300

    Feat: escrever arquivos
    :)

--- EP1_FINAL.asm
+++ EP1_FINAL.asm
@@ -5,21 +5,22 @@
     yTest: .asciiz "Ytest.txt" # Localizacao do arquivo de entrada (y) do conjunto de teste
 
     buffer: .space 200000
+    numeroTeste: .double 123.456
 
+    numero: .space 8 # Variavel para armazenar os digitos do numero para a escrita ("buffer")
+    newline: .asciiz "\n"
+    
     .align 3
     v_xTrain: .space 40000
     v_yTrain: .space 40000
     v_xTest: .space 40000
     v_yTest: .space 40000
-    
+
     bufferSize: .word 200000
     dezDouble: .double 10.0 
     zeroDouble: .double 0.0
     fimDouble: .double -1.0
     
-    temp: .space 8 # Variavel temporaria para armazenar valores
-    
-    #masValue: .double 999999999999999
 
 .text
 .globl main
@@ -32,7 +33,7 @@ main:
     jal lerArquivo
 
     la $a0, yTest
-    la $a3, v_xTrain
+    la $a3, v_yTest
     jal escreverArquivo
 
 
@@ -69,8 +70,8 @@ processarNum:
     la $s0, ($a3) # Carrega o endereco do vetor onde os valores serao armazenados
     la $s1, buffer # Carrega o buffer em si
     li $t1, 0 # Inicializa o registrador que irá conter o número copiado
-    l.d $f10, dezDouble # Inicializa o registrador com 10.0 - NÃO PODE USAR,  TROCAR DEPOIS
-    l.d $f0, zeroDouble # Inicializa o registrador com 0.0 - NÃO PODE USAR, TROCAR DEPOIS
+    ldc1 $f10, dezDouble # Inicializa o registrador com 10.0 - NÃO PODE USAR,  TROCAR DEPOIS
+    ldc1 $f0, zeroDouble # Inicializa o registrador com 0.0 - NÃO PODE USAR, TROCAR DEPOIS
     
 
 copiaNumero:
@@ -119,9 +120,9 @@ fimNumero:
     
 
     # TESTE-Imprime o valor
-    #move $a0, $s0 # We can pass the address of the array element that contains the double value
-    #l.d $f12, 0($a0) # Load the double value from memory to $f12
-    #li $v0, 3 # Syscall code for printing a floating-point number
+    #move $a0, $s0
+    #ldc1 $f12, 0($a0)
+    #li $v0, 3
     #syscall
 
 
@@ -131,21 +132,21 @@ fimNumero:
     li $t1, 0
     li $t2, 0
     li $t3, 0
-    l.d $f0, zeroDouble
-    l.d $f2, zeroDouble
+    ldc1 $f0, zeroDouble
+    ldc1 $f2, zeroDouble
 
     beqz $t0, fimArquivo # Verifica se o arquivo chegou ao fim
 
     j fimCaractere # Avança para o próximo número
 
 fimArquivo:
-    l.d $f2, fimDouble
+    ldc1 $f2, fimDouble
     s.d $f2, 0($s0) # Guarda o valor no vetor
 
     # TESTE-Imprime o valor (só para o -1)
 
     #move $a0, $s0
-    #l.d $f12, 0($a0)
+    #ldc1 $f12, 0($a0)
     #li $v0, 3
     #syscall
 
@@ -163,6 +164,97 @@ fimArquivo:
     
     jr $ra
 
+escreverArquivo:
+
+    move $s0, $a3 # Guarda o endereco do vetor
+    la $s1, numero # Guarda o endereco da variavel que armazenara os digitos do numero
+
+    #syscall de abertura para criar o arquivo
+    li $v0, 13 # Syscall 13: Abre um arquivo
+    li $a1, 1 # Flag 1: Write-only
+    li $a2, 0 # Modo que libera as permissoes de acesso
+    syscall
+    move $t0, $v0 # Salva o descritor do arquivo em t0
+
+
+escrita:
+    ldc1 $f2, 0($s0)
+    ldc1 $f4, fimDouble
+    c.eq.d $f2, $f4 # Verifica se o array acabou de ser lido (-1.0 é a sentinela)
+    bc1t fimEscrita # Se sim, finaliza a escrita
+
+
+
+    dividirAlgarismos:
+        mul.d $f2, $f2, $f10 # Multiplica o numero por 100 para obter o numero inteiro e seus dois digitos decimais
+        mul.d $f2, $f2, $f10
+
+        cvt.w.d $f2, $f2 # Converte o numero para inteiro
+        mfc1 $t1, $f2
+
+        la $s1, numero # Carrega o endereco da variavel que armazenara os digitos do numero
+        li $t5, 0 # Inicializa o contador de digitos
+
+        loopConversaoEscrita:
+            beq $t5, 2, escreverPonto # Verifica se o contador de casas decimais chegou a 2
+            li $t2, 10 
+            div $t1, $t2 # Divide o numero por 10
+            mfhi $t4 
+            addi $t4, $t4, 48 # Converte o numero para ASCII
+            sb $t4, 0($s1) # Armazena o numero no buffer
+
+            addi $s1, $s1, 1 # Avanca o ponteiro do numero
+            addi $t5, $t5, 1 # Incrementa o contador de digitos
+            mflo $t1 # Atualiza o numero
+            bnez $t1, loopConversaoEscrita # Verifica se o numero ainda possui digitos
+
+    escreverNumero:
+
+        beq $t5, 0, proximoNumeroEscrita # Verifica se o numero possui digitos restantes
+
+        la $a3, numero 
+        add $a3, $a3, $t5 # Atualiza o endereco do ponteiro do numero
+        add $a3, $a3, -1 # Volta uma posicao para ajustar a posição do ponteiro (uma anterior do contador de digitos)
+
+        
+        
+        move $a1, $a3 # Endereco do buffer
+        li $a2, 1 # Tamanho do caractere lido do array numero para ser escrito
+        move $a0, $t0 # Usa o descritor do arquivo de t0
+        li $v0, 15 # Syscall 15: Escreve no arquivo
+
+        syscall
+
+        addi $s1, $s1, -1
+        addi $t5, $t5, -1
+
+        j escreverNumero
+    
+    escreverPonto:
+        li $t4, 46 # ASCII do ponto
+        sb $t4, 0($s1) # Armazena o ponto no array temporário de números
+        addi $s1, $s1, 1 # Avança o ponteiro
+        addi $t5, $t5, 1 # Incrementa o contador de digitos
+        j loopConversaoEscrita # Volta para a conversao de digitos
+
+proximoNumeroEscrita:
+    addiu $s0, $s0, 8  # Avança para o próximo número
+    lw $t4, newline # Adiciona uma quebra de linha
+    sb $t4, 0($s1) 
+
+    move $a1, $s1
+    li $a2, 1
+    move $a0, $t0
+    li $v0, 15 # Syscall 15: Escreve no arquivo
+    syscall
+
+    j escrita
+
+fimEscrita:
+    li $v0, 16 #Fecha o arquivo
+    syscall
+
+    jr $ra
 
 fim: 
     li $v0, 10
--- teste.asm
+++ teste.asm
@@ -1,14 +1,26 @@
-.data 
-    coiso: .double 55324.7
-    coiso2: .double 10.0
+.data
+    path: .asciiz "teste.txt"
+    teste: .space 2
 
 .text
-.globl main
-
-main:
-    l.d $f0, coiso
-    l.d $f2, coiso2
-    div.d $f4, $f0, $f2
-    round.w.d $f4, $f0
-    cvt.d.w $f4, $f4
-
+file_open:
+    li $v0, 13
+    la $a0, path
+    li $a1, 1
+    li $a2, 0
+    syscall  # File descriptor gets returned in $v0
+    
+file_write:
+    move $a0, $v0  # Syscall 15 requieres file descriptor in $a0
+    li $v0, 15
+    li $t1, 57
+    la $s0, teste
+    sb $t1, 0($s0)
+    addi $s0, $s0, 1
+    sb $t1, 0($s0)
+    la $a1, teste
+    la $a2, 2
+    syscall
+file_close:
+    li $v0, 16  # $a0 already has the file descriptor
+    syscall
